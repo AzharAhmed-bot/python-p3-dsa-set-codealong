1b624bacd47f9d3a911e3567a89e276eb322b113
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-06-03 10:22:03 +0300

    Complete assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,22 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+    def has(self,value):
+        return value in self.dictionary
+    
+    def add(self,value):
+        self.dictionary[value]=True
+        return self
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+    def size(self):
+        return len(self.dictionary)
+    def clear(self):
+        self.dictionary.clear()
 
-    pass
+
+mtlist=MySet([1,2,3,4,5,5,6,7,8,9])
+print(mtlist.add(3))
